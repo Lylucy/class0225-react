cdfde8759bab75adc5d37122350ab5e1a724e7bb
yanyuchao <1769446937@qq.com>
2019-06-26 08:54:58 +0800

    提交表格代码

--- src/api/index.js
+++ src/api/index.js
@@ -14,33 +14,50 @@ import ajax from './ajax';
  */
 export const reqLogin = (username, password) => ajax('/login', {username, password}, 'POST');
 
+export const reqValidateUserInfo = (id) => ajax('/validate/user', {id}, 'POST');
 /**
  * 请求验证用户信息
  * @param id
  * @returns {返回值一定成功状态promise（请求成功里面有数据，请求失败里面没有）}
  */
-export const reqValidateUserInfo = (id) => ajax('/validate/user', {id}, 'POST');
 
 /**
  * 请求天气
  * @returns {Promise<any>}
  */
 export const reqWeather = function () {
-  return new Promise((resolve, reject) => {
-    jsonp(`http://api.map.baidu.com/telematics/v3/weather?location=深圳&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`, {}, function (err, data) {
-      if (!err) {
-        const { dayPictureUrl, weather } = data.results[0].weather_data[0];
-        resolve({
-          weatherImg: dayPictureUrl,
-          weather
-        });
-      } else {
+  let cancel = null;
+  const promise = new Promise((resolve, reject) => {
+    cancel = jsonp(`http://api.map.baidu.com/telematics/v3/weather?location=深圳&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`, {}, function (err, data) {
+      try {
+        if (!err) {
+          const { dayPictureUrl, weather } = data.results[0].weather_data[0];
+          resolve({
+            weatherImg: dayPictureUrl,
+            weather
+          });
+        } else {
+          message.error('请求天气信息失败~请刷新试试~');
+          resolve();
+        }
+      } catch (e) {
         message.error('请求天气信息失败~请刷新试试~');
         resolve();
       }
     });
   });
+
+  return {
+    promise,
+    cancel
+  }
 };
 
+
+
 export const reqCategories = (parentId) => ajax('/manage/category/list', {parentId});
+
+export const reqAddCategory = (parentId, categoryName) => ajax('/manage/category/add', {parentId, categoryName}, 'POST');
+
+export const reqUpdateCategoryName = (categoryId, categoryName) => ajax('/manage/category/update', {categoryId, categoryName}, 'POST');
 
--- src/pages/category/index.jsx
+++ src/pages/category/index.jsx
@@ -1,51 +1,222 @@
 import React, { Component } from 'react';
-import { Card, Button, Icon, Table } from 'antd';
+import { Card, Button, Icon, Table, Modal, message } from 'antd';
 
-import { reqCategories } from '../../api';
+import { reqCategories, reqAddCategory, reqUpdateCategoryName  } from '../../api';
 import MyButton from '../../components/my-button';
+import AddCategoryForm from './add-category-form';
+import UpdateCategoryNameForm from './update-category-name';
 import './index.less';
 
 export default class Category extends Component{
   state = {
     categories: [],//一级分类列表
+    isShowAddCategory: false, // 显示添加品类
+    isShowUpdateCategoryName: false, // 显示修改分类名称
+  };
 
-  }
+  category = {};
 
   async componentDidMount(){
     const result = await reqCategories('0');
     if (result) {
       this.setState({categories: result});
     }
-  }
-  render(){
-    //决定表头内容
+  };
+
+
+  addCategory = () => {
+    // 1. 表单校验
+    // 2. 收集表单数据
+    // console.log(this.addCategoryForm);
+    const { form } =this.addCategoryForm.props;
+
+    form.validateFields(async (err, values) => {
+      if (!err) {
+        //校验通过
+        console.log(values);
+        const { parentId, categoryName } = values;
+        //3.发送请求
+        const result = await reqAddCategory(parentId, categoryName);
+        if (result) {
+          //添加分类成功
+          message.success('添加分类成功', 2);
+          //清空表单数据
+          form.resetFields(['parentId', 'categoryName']);
+
+          const options = {
+            isShowAddCategory:false
+          };
+
+          if (result.parentId === '0') {
+            options.categories = [...this.state.categories, result];
+
+          }
+          //统一更新
+          this.setState(options);
+        }
+      }
+    })
+  };
+
+  /**
+   * 切换显示
+   * @returns {*}
+   */
+  toggleDisplay = (stateName, stateValue) => {
+    return () => {
+      this.setState({
+        [stateName]:stateValue
+      })
+    }
+  };
+
+  hideUpdateCategoryName = () => {
+    //清空表单项的值
+    this.hideUpdateCategoryNameForm.props.form.resetFields(['categoryName']);
+    //隐藏对话框
+    this.setState({
+      isShowUpdateCategoryName: false
+    })
+  };
+
+  saveCategory = (category) => {
+    return () => {
+      //保存更新的分类数据
+      this.category = category;
+      this.setState({
+        isShowUpdateCategoryName:true
+      })
+    }
+  };
+
+
+  updateCategoryName = () => {
+    const { form } = this.updateCategoryNameForm.props;
+    //校验表单，收集数据
+    form.validateFields(async (err, values) => {
+      if (!err) {
+        const { categoryName } = values;
+        const categoryId = this.category._id;
+        //发送请求
+        const result = await reqUpdateCategoryName(categoryId,categoryName)
+;
+        if (result) {
+          //不想修改源数据
+          const categories = this.state.categories.map((category) => {
+            let { _id, name,parentId } = category;
+            //找到对应id的category，修改分类名称
+            if (_id === categoryId) {
+              name = categoryName;
+              return {
+                _id,
+                name,
+                parentId
+              }
+            }
+            //没有修改的数据直接返回
+            return category
+          });
+
+          //清空表单项的值 隐藏对话框
+          form.resetFields(['categoryName']);
+          message.success('更新分类名称成功',2 );
+
+          this.setState({
+            isShowUpdateCategoryName:false,
+            categories
+          })
+        }
+      }
+    })
+  };
+
+
+
+
+  render() {
+    const { categories, isShowAddCategory, isShowUpdateCategoryName } = this.state;
+
+    // 决定表头内容
     const columns = [
       {
-        title:'品类名称',
-        dataIndex: 'operation',
+        title: '品类名称',
+        dataIndex: 'name',
+      },
+      {
+        title: '操作',
+        // dataIndex: '_id',
         className: 'category-operation',
-        //改变当前的显示
-        render: text => {
+        // 改变当列的显示
+        render: category => {
+          // console.log(category);
           return <div>
-            <MyButton>修改名称</MyButton>
+            <MyButton onClick={this.saveCategory(category)}>修改名称</MyButton>
             <MyButton>查看其子品类</MyButton>
           </div>
         },
       },
     ];
-    //决定表格里面的数据
-    return <Card title="一级分类列表" extra={<Button type="primary"><Icon type="plus" />添加品类</Button>}>
+    // 决定表格里面数据
+    /*const data = [
+      {
+        key: '1',
+        name: '手机',
+        // operation: 'xxxxx',
+      },
+      {
+        key: '2',
+        categoryName: '电脑',
+        // operation: 'yyyy',
+      },
+      {
+        key: '3',
+        categoryName: '耳机',
+        // operation: 'zzzzzz',
+      },
+      {
+        key: '4',
+        categoryName: '鼠标',
+        // operation: 'zzzzzz',
+      },
+    ];*/
+
+    return <Card title="一级分类列表" extra={<Button type="primary" onClick={this.toggleDisplay('isShowAddCategory', true)}><Icon type="plus" />添加品类</Button>}>
       <Table
         columns={columns}
-        dataSource={this.state.categories}
+        dataSource={categories}
         bordered
         pagination={{
           showSizeChanger: true,
-          pageSizeOptions: ['3','6','9', '12'],
+          pageSizeOptions: ['3', '6', '9', '12'],
           defaultPageSize: 3,
           showQuickJumper: true
         }}
+        rowKey="_id"
       />
-    </Card>
+
+      <Modal
+        title="添加分类"
+        visible={isShowAddCategory}
+        onOk={this.addCategory}
+        onCancel={this.toggleDisplay('isShowAddCategory', false)}
+        okText="确认"
+        cancelText="取消"
+      >
+        <AddCategoryForm categories={categories} wrappedComponentRef={(form) => this.addCategoryForm = form}/>
+      </Modal>
+
+      <Modal
+        title="修改分类名称"
+        visible={isShowUpdateCategoryName}
+        onOk={this.updateCategoryName}
+        onCancel={this.hideUpdateCategoryName}
+        okText="确认"
+        cancelText="取消"
+        width={250}
+      >
+        <UpdateCategoryNameForm categoryName={this.category.name} wrappedComponentRef={(form) => this.updateCategoryNameForm = form}/>
+      </Modal>
+
+    </Card>;
   }
 }
